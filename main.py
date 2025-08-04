364eb6886c494a69c86684f43e98b40a7f2a69f2
Mahdi Alsalami <mahdialsalami0@gmail.com>
2025-08-04 16:45:56 -0700

    created fps for window and removed comments

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f" Screen width: {constants.SCREEN_WIDTH}")
-    print(f" Screen height: {constants.SCREEN_HEIGHT}")
+    
+    dt = 0
+    delta = pygame.time.Clock()
+
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        black = (0, 0, 0)
-        screen.fill(black)
+        
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = delta.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
