6bae7a464ae869028d91320af27ec6778ec602cd
MahdiAlsalami <mahdialsalami0@gmail.com>
2025-08-06 23:15:11 -0700

    created a group class to mamange game objects

--- main.py
+++ main.py
@@ -7,22 +7,34 @@ def main():
     pygame.init()
     
     dt = 0
+   
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     delta = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
         player.update(dt)
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         player.draw(screen)
         pygame.display.flip()
 
         dt = delta.tick(60) / 1000 
-
 
         
 
